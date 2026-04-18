36f78159d4299d2f73e709444706192459d908b6
acjtechnologies-cpu <acjtechnologies.cpu@gmail.com>
2026-04-18 10:20:50 +0200

    fix: main.jsx - reset localStorage corrompu au demarrage v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+﻿// Reset localStorage si version app change
+const APP_VERSION = '2'
+if (localStorage.getItem('mbi_version') !== APP_VERSION) {
+  localStorage.clear()
+  localStorage.setItem('mbi_version', APP_VERSION)
+}
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
